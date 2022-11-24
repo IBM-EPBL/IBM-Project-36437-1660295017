593a99cca7ff15187811069cf188806db8c998e3
KarunyaSweetlin <113575438+KarunyaSweetlin@users.noreply.github.com>
2022-11-24 17:57:46 +0530

    Add files via upload

---
+++ Final Deliverables/Python/app.py
@@ -0,0 +1,119 @@
+from turtle import st
+from flask import Flask, render_template, request, redirect, url_for, session
+from markupsafe import escape
+import os
+
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=0c77d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31198;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=rvq09623;PWD=pI2a185OYJIchgp8",'','')
+print(conn)
+print("connection successful...")
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
+
+@app.route('/signin')
+def signin():
+  return render_template('signin.html')
+
+@app.route('/product.html')
+def product():
+  return render_template('product.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+  if request.method == 'POST':
+
+    name = request.form['name']
+    mail = request.form['number']
+    city = request.form['psw']
+    pin = request.form['psw-repeat']
+
+    sql = "SELECT * FROM user WHERE name =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,name)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+
+    if account:
+      return render_template('list.html', msg="You are already a member, please login using your details")
+    else:
+      insert_sql = "INSERT INTO user VALUES (?,?,?,?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 1, name)
+      ibm_db.bind_param(prep_stmt, 2, mail)
+      ibm_db.bind_param(prep_stmt, 3, city)
+      ibm_db.bind_param(prep_stmt, 4, pin)
+      ibm_db.execute(prep_stmt)
+    
+    return render_template(' new home.html', msg="user Data saved successfuly..")
+
+@app.route('/list')
+def list():
+  students = []
+  sql = "SELECT * FROM user"
+  stmt = ibm_db.exec_immediate(conn, sql)
+  dictionary = ibm_db.fetch_both(stmt)
+  while dictionary != False:
+    # print ("The Name is : ",  dictionary)
+    students.append(dictionary)
+    dictionary = ibm_db.fetch_both(stmt)
+
+  if students:
+    return render_template("list.html", students = students)
+
+@app.route('/delete/<name>')
+def delete(name):
+  sql = f"SELECT * FROM Students WHERE name='{escape(name)}'"
+  print(sql)
+  stmt = ibm_db.exec_immediate(conn, sql)
+  student = ibm_db.fetch_row(stmt)
+  print ("The Name is : ",  student)
+  if student:
+    sql = f"DELETE FROM Students WHERE name='{escape(name)}'"
+    print(sql)
+    stmt = ibm_db.exec_immediate(conn, sql)
+    students = []
+    sql = "SELECT * FROM Students"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_both(stmt)
+    while dictionary != False:
+      students.append(dictionary)
+      dictionary = ibm_db.fetch_both(stmt)
+    if students:
+      return render_template("list.html", students = students, msg="Delete successfully")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+  
+  # # while student != False:
+  # #   print ("The Name is : ",  student)
+
+  # print(student)
+  
+
+# @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+# def edit(id):
+    
+#     post = BlogPost.query.get_or_404(id)
+
+#     if request.method == 'POST':
+#         post.title = request.form['title']
+#         post.author = request.form['author']
+#         post.content = request.form['content']
+#         db.session.commit()
+#         return redirect('/posts')
+#     else:
+#         return render_template('edit.html', post=post)
